7ca01a5f212a50f3596efebe8d12e69c00ca7de6
romankanterukmtrim2025-afk <roman.kanteruk.mtrim.2025@lpnu.ua>
2026-05-21 11:39:16 +0300

    Add files via upload

--- src/main/java/roman_dst/MyMain.java
+++ src/main/java/roman_dst/MyMain.java
@@ -1,10 +1,33 @@
 package roman_dst;
 
+import java.util.Arrays;
+import java.util.Scanner;
+
 public class MyMain {
+    public static void main(String[] args) {
+        System.out.println("HELLo World!");
+        Scanner scanner = new Scanner(System.in);
+        
+        while (true) {
+            String line = scanner.nextLine().trim();
+            if (line.isEmpty()) continue;
+            
+            String[] parts = line.split("\\s+", 2);
+            String command = parts[0];
+            String[] params;
 
-	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-		System.out.println("HELLo World!");
-	}
+            if (command.equals("echo") && parts.length > 1) {
+                params = new String[]{parts[1]};
+            } else if (parts.length > 1) {
+                params = parts[1].split("\\s+");
+            } else {
+                params = new String[]{};
+            }
 
+            System.out.println("Entered command = \"" + command + "\", parameters = " + Arrays.toString(params));
+
+            if (command.equals("exit")) break;
+        }
+        scanner.close();
+    }
 }
